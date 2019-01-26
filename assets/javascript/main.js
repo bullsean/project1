fd71b2e5076e3bebb975ec26461bafc375ac9a98
bullsean <41093836+bullsean@users.noreply.github.com>
2019-01-26 10:40:02 -0600

    added additional APIs

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,9 @@
 $("#weather").on("click", function() {
     var triviaQueryURL = "https://opentdb.com/api.php?amount=10";
     var jokeQueryURL = "https://icanhazdadjoke.com/";
-    var weatherCity = $("#cityName").val();
-    var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?appid=029b688e6e7c61bcc27ad9ebfa0f39a6&q=" + weatherCity;
+    var city = $("#cityName").val();
+    var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?appid=029b688e6e7c61bcc27ad9ebfa0f39a6&q=" + city;
+    var zomatoQueryURL = "https://developers.zomato.com/api/v2.1/search?q=" + city
 
     $.ajax({
         url: triviaQueryURL,
@@ -26,6 +27,16 @@ $("#weather").on("click", function() {
     $.ajax({
         url: weatherQueryURL,
         method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
+
+    $.ajax({
+        url: zomatoQueryURL,
+        method: "GET",
+        headers: {
+            "user-key": "a7aef2e9343cea29c41366c8092a682f"
+        }
     }).then(function(response) {
         console.log(response);
     });
