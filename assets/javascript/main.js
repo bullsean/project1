8d7a53b76b5cc42136588c12faea8bedcc778b21
zohaibdhukka <zohaibdhukka@gmail.com>
2019-01-28 20:33:18 -0600

    did some styling and fix news columns

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,12 @@
 $("#submit-button").on("click", function() {
+    $('.card-columns').html('');
     var triviaQueryURL = "https://opentdb.com/api.php?amount=10";
     var jokeQueryURL = "https://icanhazdadjoke.com/";
     var city = $("#city-input").val();
     var keyword = $("#keyword-input").val();
     var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?appid=029b688e6e7c61bcc27ad9ebfa0f39a6&q=" + city;
     var zomatoQueryURL = "https://developers.zomato.com/api/v2.1/search?q=" + city;
-    var newsUrl = 'https://newsapi.org/v2/top-headlines?country=us&q='+ keyword +'&category=business&apiKey=7366515f5173476eb141e59de078bc65';
+    var newsUrl = 'https://newsapi.org/v2/top-headlines?source=bbc-news&q='+ keyword +'&apiKey=7366515f5173476eb141e59de078bc65';
     $.ajax({
         url: triviaQueryURL,
         method: "GET"
@@ -52,13 +53,13 @@ $("#submit-button").on("click", function() {
 
             var img = ("<img src=" +response.articles[j].urlToImage + "><br>");
             var link = ("Link: <a href=" + response.articles[j].url + " target='_blank'> Click to view full news</a><br>");
-            var content = ("<p>Content: " +response.articles[j].title + "</p>"+"<br>");
+            var title = ("<p style='font-weight: bolder;'>Title: " +response.articles[j].title + "</p>"+"<br>");
             var description = ("Description: " +response.articles[j].description + "<br>");
             var published = ("Published on: " +response.articles[j].publishedAt + "<br>");
             var source = ("<p>Source: " +response.articles[j].source.name + "</p>"+"<br>");
             
             
-            var addRow = ("<div class='card-deck'><div class='card'>" + source  + img + "<div class='card-body'><h5 class='card-title'>" + content + "</h5><p class='card-text'>" + description + "</p><p class='card-text'>" + link + "</p><div class='card-footer'><small class='text-muted'>" + published + "</small></div></div></div></div><br><br>")
+            var addRow = ("<div class='card'>"+ title + img + "<div class='card-body'><h5 class='card-title'>" + description + "</p><p class='card-text'>" + link + "</p></div></div>")
 
                 
             $("#row-news").append(addRow);
