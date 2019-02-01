03080a09b1bbffea8360a4e4fbb0feed2823edcd
bullsean <41093836+bullsean@users.noreply.github.com>
2019-01-31 19:06:36 -0600

    Just committing changes:

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,4 @@
-$("#submit-button").on("click", function() {
+$("#submit-button").on("click", function () {
     $("#input-search").addClass("fadeOutUpBig");
     $("#content").addClass("fadeInUpBig");
     $("#content").attr('style', "margin-top: -100vh;");
@@ -9,13 +9,13 @@ $("#submit-button").on("click", function() {
     var city = $("#city-input").val();
     var keyword = $("#keyword-input").val();
     var zomatoQueryURL = "https://developers.zomato.com/api/v2.1/search?q=" + city;
-    var newsUrl = 'https://newsapi.org/v2/top-headlines?source=bbc-news&q='+ keyword +'&apiKey=7366515f5173476eb141e59de078bc65';
+    var newsUrl = 'https://newsapi.org/v2/top-headlines?source=bbc-news&q=' + keyword + '&apiKey=7366515f5173476eb141e59de078bc65';
     var weatherQueryURL = "https://api.openweathermap.org/data/2.5/forecast?appid=029b688e6e7c61bcc27ad9ebfa0f39a6&q=" + city;
 
     $.ajax({
         url: triviaQueryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
         console.log(response);
     });
 
@@ -23,41 +23,41 @@ $("#submit-button").on("click", function() {
         url: jokeQueryURL,
         method: "GET",
         headers: {
-           "Accept" : "application/json"
+            "Accept": "application/json"
         }
-    }).then(function(joke) {
+    }).then(function (joke) {
         console.log(joke);
         $("#row-joke").append("<div class='card'><h2>Dad Joke</h2><div class='card-body'><p>" + joke.joke + "</p></div></div>")
 
-                
-            
+
+
     });
 
 
     $.ajax({
         url: weatherQueryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
         console.log(response);
-        
 
-        for (var i = 0; i<response.list.length ; i++) {
+
+        for (var i = 0; i < response.list.length; i++) {
             if (i === 3 || i === 11 || i === 19 || i === 27 || i === 35) {
                 console.log('This is loop number' + i);
                 console.log(response.list[i].main.temp)
                 var kelTemperature = response.list[i].main.temp;
                 var weatherType = response.list[0].weather[0].description;
-                
+
                 function tempConversion(kelTemperature) {
-                    return Math.ceil(((kelTemperature - 273.15) * 1.80) +32);
+                    return Math.ceil(((kelTemperature - 273.15) * 1.80) + 32);
                 };
-                var weatherDiv = $('<div class="card" style="width: 8rem;"><div id="weatherPicDay'+ i +'" style="width: 50px"></div><div class="card-body"><h5 class="card-title" id="weatherDay'+ i +'-title" style="color: black"></h5><p class="card-text" id="weatherDate'+ i +'" style="color: black"></p></div></div>');
+                var weatherDiv = $('<div class="card" style="width: 8rem;"><div id="weatherPicDay' + i + '" style="width: 50px"></div><div class="card-body"><h5 class="card-title" id="weatherDay' + i + '-title" style="color: black"></h5><p class="card-text" id="weatherDate' + i + '" style="color: black"></p></div></div>');
                 $("#row-weather").append(weatherDiv);
-                $('#weatherDay'+ i +'-title').text(tempConversion(kelTemperature) + "°F");
-                $('#weatherPicDay'+ i +'').html('<img class="card-img-top" src="https://openweathermap.org/img/w/' + response.list[i].weather[0].icon +'.png" alt="Card image cap">');
-                $('#weatherDate'+ i +'').text(moment(response.list[i].dt_txt).format('ddd, MMM D'));
+                $('#weatherDay' + i + '-title').text(tempConversion(kelTemperature) + "°F");
+                $('#weatherPicDay' + i + '').html('<img class="card-img-top" src="https://openweathermap.org/img/w/' + response.list[i].weather[0].icon + '.png" alt="Card image cap">');
+                $('#weatherDate' + i + '').text(moment(response.list[i].dt_txt).format('ddd, MMM D'));
                 $("#weatherQ").text(weatherType);
-                
+
             }
         }
 
@@ -70,35 +70,35 @@ $("#submit-button").on("click", function() {
         headers: {
             "user-key": "a7aef2e9343cea29c41366c8092a682f"
         }
-    }).then(function(response) {
+    }).then(function (response) {
         console.log(response);
     });
 
 
     $.ajax({
-        url:newsUrl,
+        url: newsUrl,
         method: "GET"
-    }).then (function(response){
+    }).then(function (response) {
         console.log(response);
         for (var j = 0; j < response.articles.length; j++) {
 
-            var img = ("<img src=" +response.articles[j].urlToImage + "><br>");
+            var img = ("<img src=" + response.articles[j].urlToImage + "><br>");
             var link = ("Link: <a href=" + response.articles[j].url + " target='_blank'> Click to view full news</a><br>");
-            var title = ("<p style='font-weight: bolder;'>Title: " +response.articles[j].title + "</p>"+"<br>");
-            var description = ("Description: " +response.articles[j].description + "<br>");
-            var published = ("Published on: " +response.articles[j].publishedAt + "<br>");
-            var source = ("<p>Source: " +response.articles[j].source.name + "</p>"+"<br>");
-            
-            
-            var addRow = ("<div class='card'>"+ title + img + "<div class='card-body'><h5 class='card-title'>" + description + "</p><p class='card-text'>" + link + "</p></div></div>")
+            var title = ("<p style='font-weight: bolder;'>Title: " + response.articles[j].title + "</p>" + "<br>");
+            var description = ("Description: " + response.articles[j].description + "<br>");
+            var published = ("Published on: " + response.articles[j].publishedAt + "<br>");
+            var source = ("<p>Source: " + response.articles[j].source.name + "</p>" + "<br>");
 
-                
+
+            var addRow = ("<div class='card'>" + title + img + "<div class='card-body'><h5 class='card-title'>" + description + "</p><p class='card-text'>" + link + "</p></div></div>")
+
+
             $("#row-news").append(addRow);
-                
-               
-            
 
-          }
+
+
+
+        }
 
     })
 
